a186b1ad4776eb666b7871932e10409b4d3adea4
David Osornio <davidosornio@gmail.com>
2016-06-13 00:36:43 -0500

    Initial Commit

---
+++ MiniReto1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniReto1
+//
+//  Created by Alfredo on 13/06/16.
+//  Copyright © 2016 Alfredo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
